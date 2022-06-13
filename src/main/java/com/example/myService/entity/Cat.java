0c064bbf44c6b8cf4632953086ed53d5ebaeb6d8
ZeroOneKim <Wowwow0124@naver.com>
2022-06-14 04:07:17 +0900

    엔티티 생성

--- src/main/java/com/example/myService/entity/Cat.java
+++ src/main/java/com/example/myService/entity/Cat.java
@@ -1,4 +1,13 @@
 package com.example.myService.entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class Cat {
+    @Id
+    public String cat_id;
 }
