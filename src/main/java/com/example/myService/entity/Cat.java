c48a155f60f673c1a9d4cb4c5ffbfe7ae7b2cbd5
ZeroOneKim <Wowwow0124@naver.com>
2022-06-14 04:20:53 +0900

    레파지토리 생성

--- src/main/java/com/example/myService/entity/Cat.java
+++ src/main/java/com/example/myService/entity/Cat.java
@@ -3,11 +3,24 @@ package com.example.myService.entity;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 @Data
 public class Cat {
     @Id
-    public String cat_id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cat_id;
+
+    private String cat_species;
+    private String cat_age;
+    private String cat_wm;
+    private String cat_vaccine;
+    private String cat_neutral;
+    private String cat_about;
+
+    private String cat_filename;
+    private String cat_filepath;
 }
