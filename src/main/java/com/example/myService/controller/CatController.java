a8711e755fc3639be565a2c0268ad964585f0890
ZeroOneKim <Wowwow0124@naver.com>
2022-06-15 00:35:03 +0900

    연결

--- src/main/java/com/example/myService/controller/CatController.java
+++ src/main/java/com/example/myService/controller/CatController.java
@@ -1,6 +1,9 @@
 package com.example.myService.controller;
 
+import com.example.myService.repository.CatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,10 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/cat")
 public class CatController {
 
+    @Autowired
+    private CatRepository catRepository;
+
     @GetMapping("/catlist")
-    public String list() {
+    public String list(Model model) {
+        catRepository.findAll();
         return "cat/catlist";
     }
-
-
 }
