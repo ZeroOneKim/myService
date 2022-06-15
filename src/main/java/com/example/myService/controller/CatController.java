6940b18e84be0a73376fdc5a5f28cebbbbb653bb
ZeroOneKim <Wowwow0124@naver.com>
2022-06-16 00:11:13 +0900

    CatView 설정

--- src/main/java/com/example/myService/controller/CatController.java
+++ src/main/java/com/example/myService/controller/CatController.java
@@ -1,6 +1,7 @@
 package com.example.myService.controller;
 
 import com.example.myService.entity.Cat;
+import com.example.myService.repository.CatRepository;
 import com.example.myService.service.CatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,12 +9,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/cat")
 public class CatController {
-
+    @Autowired
+    private CatRepository catRepository;
     @Autowired
     private CatService catService;
 
@@ -31,6 +34,17 @@ public class CatController {
     public String catformpro(Cat cat, Model model, MultipartFile catfile) throws Exception {
         catService.catwrite(cat, catfile);
         return "redirect:/cat/catlist";
+    }
+
+    @GetMapping("/catview")
+    public String catView(Model model, @RequestParam(required = false ) Long id) {
+        if (id==null) {
+            model.addAttribute("cat", new Cat());
+        } else {
+            Cat cat = catRepository.findById(id).orElse(null);
+            model.addAttribute("cat", cat);
+        }
+        return "/cat/catview";
     }
 
 }
