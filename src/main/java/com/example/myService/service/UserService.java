2da6a90a9068c8a65fbd0e0240725342816ebb97
ZeroOneKim <Wowwow0124@naver.com>
2022-06-19 21:32:41 +0900

    add about Register

---
+++ src/main/java/com/example/myService/service/UserService.java
@@ -0,0 +1,26 @@
+package com.example.myService.service;
+
+import com.example.myService.entity.Role;
+import com.example.myService.entity.User;
+import com.example.myService.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    public User save(User user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        user.setEnabled(true);
+
+        Role role = new Role();
+        role.setId(1l);
+        user.getRoles().add(role);
+        return userRepository.save(user);
+    }
+}
