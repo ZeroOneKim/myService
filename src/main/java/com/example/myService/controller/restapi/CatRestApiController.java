3cfb646702d11640f17337f7787c626355e86408
ZeroOneKim <Wowwow0124@naver.com>
2022-06-24 17:24:11 +0900

    S

--- src/main/java/com/example/myService/controller/restapi/CatRestApiController.java
+++ src/main/java/com/example/myService/controller/restapi/CatRestApiController.java
@@ -3,6 +3,7 @@ package com.example.myService.controller.restapi;
 import com.example.myService.entity.Cat;
 import com.example.myService.repository.CatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;          //아직 미구현 ㅠㅠ...
