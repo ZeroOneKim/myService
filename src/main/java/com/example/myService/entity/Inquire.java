8512c0050b6ec275f86bbcad2ab99f7fb1358049
ZeroOneKim <Wowwow0124@naver.com>
2022-06-27 04:15:38 +0900

    문의 DB Make

---
+++ src/main/java/com/example/myService/entity/Inquire.java
@@ -0,0 +1,23 @@
+package com.example.myService.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Inquire {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long inquire_id;
+
+    // user FK
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // cat FK
+    @ManyToOne
+    @JoinColumn(name = "cat_id")
+    private Cat cat;
+}
---
+++ src/main/java/com/example/myService/repository/InquireRepository.java
@@ -0,0 +1,7 @@
+package com.example.myService.repository;
+
+import com.example.myService.entity.Inquire;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InquireRepository extends JpaRepository<Inquire, Long> {
+}
