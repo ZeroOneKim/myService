2365cc1ea1886ac3b3edc433922b8c2f080fed88
ZeroOneKim <Wowwow0124@naver.com>
2022-07-10 20:07:44 +0900

    drop

--- src/main/java/com/example/myService/controller/restapi/CatNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.myService.controller.restapi;
-
-public class CatNotFoundException extends RuntimeException {
-    public CatNotFoundException(Long id) {
-        super("cat에서 찾을수 없어요" + id);
-    }
-}
